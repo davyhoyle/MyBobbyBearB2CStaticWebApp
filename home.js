41b6d29b794b5520f4679a6675d2718f6d8e932c
davyhoyle <65952622+davyhoyle@users.noreply.github.com>
2023-09-29 14:49:54 +0100

    Added js and css files

---
+++ home.js
@@ -0,0 +1,114 @@
+﻿"use strict";
+
+var issuingProviders = {
+    Unknown: 'unknown',
+    AAD: 'aad',
+    B2C: 'b2c',
+    IEF: 'ief',
+    Google: 'google',
+    MSA: 'msa'
+}
+
+var Jwt = function (encodedToken) {
+    encodedToken = encodedToken.trim();
+    this.encodedToken = encodedToken;
+    this.decodedHeader = jwt_decode(encodedToken, { header: true });
+    this.decodedClaims = jwt_decode(encodedToken.trim());
+
+    var parts = encodedToken.split('.');
+    if (parts.length < 2 || parts.length > 3)
+        throw new Error('Invalid token, there must be two or three parts.');
+
+    // Public properties
+    this.encodedHeader = parts[0];
+    this.encodedClaims = parts[1];
+    this.encodedSignature = parts[2];
+    this.hasSignature = parts.length === 3 && parts[2];
+    this.issuingProvider = getIssuingProvider(this.decodedClaims);
+
+    function getIssuingProvider(decodedClaims) {
+        var iss = 'iss';
+        var tfp = 'tfp';
+        var acr = 'acr';
+        var b2cPolicyPrefix = 'b2c_1_';
+        var iefPolicyPrefix = 'b2c_1a_';
+
+        var issValue = decodedClaims[iss];
+        if (!issValue) return '';
+
+        issValue = issValue.toLowerCase();
+        var tfpValue = decodedClaims[tfp];
+        var acrValue = decodedClaims[acr];
+
+        if ((issValue.indexOf('https://login.microsoftonline.com/') === 0 || issValue.match(/https:\/\/[^./]*\.b2clogin.com\//gi))
+            && issValue.indexOf('2.0') > -1) {
+            if ((tfpValue && tfpValue.toLowerCase().indexOf(b2cPolicyPrefix) === 0) ||
+                (acrValue && acrValue.toLowerCase().indexOf(b2cPolicyPrefix) === 0)) {
+                return issuingProviders.B2C;
+            } else if ((tfpValue && tfpValue.toLowerCase().indexOf(iefPolicyPrefix) === 0) ||
+                (acrValue && acrValue.toLowerCase().indexOf(iefPolicyPrefix) === 0)) {
+                return issuingProviders.IEF;
+            }
+        }
+
+        if ((issValue.indexOf('https://login.chinacloudapi.cn/') === 0 || issValue.match(/https:\/\/[^./]*\.b2clogin.cn\//gi))
+            && issValue.indexOf('2.0') > -1) {
+            if ((tfpValue && tfpValue.toLowerCase().indexOf(b2cPolicyPrefix) === 0) ||
+                (acrValue && acrValue.toLowerCase().indexOf(b2cPolicyPrefix) === 0)) {
+                return issuingProviders.B2C;
+            } else if ((tfpValue && tfpValue.toLowerCase().indexOf(iefPolicyPrefix) === 0) ||
+                (acrValue && acrValue.toLowerCase().indexOf(iefPolicyPrefix) === 0)) {
+                return issuingProviders.IEF;
+            }
+        }
+
+        if (issValue.indexOf('https://login.microsoftonline.com/') === 0 ||
+            issValue.indexOf('https://sts.windows.net/') === 0 ||
+            issValue.indexOf('https://login.windows.net/') === 0 ||
+            issValue.indexOf('https://login.microsoft.com/') === 0 ||
+            issValue.indexOf('https://login.microsoft.com/') === 0) {
+            return issuingProviders.AAD;
+        }
+
+        if (issValue.indexOf('https://login.chinacloudapi.cn/') === 0 ||
+            issValue.indexOf('https://sts.chinacloudapi.cn/') === 0) {
+            return issuingProviders.AAD;
+        }
+
+        if (issValue.indexOf('accounts.google.com') === 0 ||
+            issValue.indexOf('https://accounts.google.com') === 0) {
+            return issuingProviders.Google;
+        }
+
+        return issuingProviders.Unknown;
+    }
+}
+
+function getUrlFragment(fragmentId) {
+    fragmentId = fragmentId + '=';
+    if (window.location.hash && window.location.hash.length > 0) {
+        var fragmentValueStartIndex = 0;
+        var fragmentIdStartIndex = window.location.hash.indexOf(fragmentId);
+        if (fragmentIdStartIndex > -1) {
+            fragmentValueStartIndex = fragmentIdStartIndex + fragmentId.length;
+        }
+
+        if (fragmentValueStartIndex > 0) {
+            var fragmentValue = window.location.hash.substring(fragmentValueStartIndex);
+            var ampIndex = fragmentValue.indexOf('&');
+            if (ampIndex !== -1) {
+                fragmentValue = fragmentValue.substring(0, ampIndex);
+            }
+
+            return fragmentValue;
+        }
+
+        return null;
+    }
+}
+
+function decodeFragmentValue(fragmentValue) {
+    fragmentValue = fragmentValue.replace(/\+/g, '%20');
+    fragmentValue = decodeURIComponent(fragmentValue);
+    return fragmentValue;
+}
---
+++ modernizr-2.6.2.js
@@ -0,0 +1,129 @@
+﻿body {
+    padding-top: 50px;
+    padding-bottom: 20px;
+}
+
+/* 768/750, 992/970, 1200/1170*/
+@media (min-width: 868px) {
+    .container {
+        max-width: 850px;
+    }
+}
+
+@media (min-width: 1092px) {
+    .container {
+        max-width: 1070px;
+    }
+}
+
+@media (min-width: 1300px) {
+    .container {
+        max-width: 1270px;
+    }
+}
+
+@media (min-width: 1400px) {
+    .container {
+        max-width: 1370px;
+    }
+}
+
+@media (min-width: 1500px) {
+    .container {
+        max-width: 1470px;
+    }
+}
+
+/* Set padding to keep content from hitting the edges */
+.body-content {
+    padding-left: 15px;
+    padding-right: 15px;
+}
+
+/* Override the default bootstrap behavior where horizontal description lists 
+   will truncate terms that are too long to fit in the left column 
+*/
+.dl-horizontal dt {
+    white-space: normal;
+}
+
+/* Set width on the form input elements since they're 100% wide by default */
+input,
+select,
+textarea {
+    max-width: 280px;
+}
+
+#claims { 
+    padding-top: 15px;
+}
+
+#decodedToken {
+    min-height: 300px;
+}
+
+.mono {
+    font-family:monospace;
+}
+
+.prewrapbreakword {
+    white-space: pre;
+    white-space: pre-wrap;       /* Since CSS 2.1 */
+    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+    white-space: -pre-wrap;      /* Opera 4-6 */
+    white-space: -o-pre-wrap;    /* Opera 7 */
+    word-wrap: break-word;       /* Internet Explorer 5.5+ */
+}
+
+.prewrapbreakall {
+    white-space: pre;
+    white-space: pre-wrap; /* Since CSS 2.1 */
+    white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+    white-space: -pre-wrap; /* Opera 4-6 */
+    white-space: -o-pre-wrap; /* Opera 7 */
+    word-break: break-all;
+}
+
+.forcebreakword {
+    word-break: break-all;
+}
+
+#idtoken {
+    min-height: 300px;
+}
+
+#encodedToken {
+    min-height: 100px;
+}
+
+div#tokenErrorText::before,
+div#tokenErrorText::after {
+    display: none;
+}
+
+div#encodedToken::before,
+div#encodedToken::after
+{
+    display: none;
+}
+
+#issuingProviderDescription, #encodedTokenHeader {
+    padding-left: 20px;
+    font-size: 0.85em;
+}
+
+#tokenErrorPanel {
+    display: none;
+}
+
+.jwtHeader {
+    color: #B00;
+}
+
+.jwtClaims {
+    color: #00B;
+}
+
+.jwtSignature {  
+    color: #090;
+}
